74112c9a52ba0da9d5b38547c5eddaede91d64bf
almv faa <faaalmv@gmail.com>
2025-10-19 03:04:06 +0000

    dtg

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,7 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
--- src/lib/hooks/__tests__/use-collapsible.test.ts
+++ src/lib/hooks/__tests__/use-collapsible.test.ts
@@ -1,5 +1,5 @@
 
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react';
 import { useCollapsible } from '../use-collapsible';
 
 describe('useCollapsible', () => {
